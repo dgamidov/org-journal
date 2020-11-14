dfcdcb6c20dfeb4b3b627797054382e58b2a6986
Denis Gamidov <dgamidov@gmail.com>
2020-11-14 15:12:44 +0300

    Initial commit

---
+++ spec/org-journal-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OrgJournalView from '../lib/org-journal-view';
+
+describe('OrgJournalView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
